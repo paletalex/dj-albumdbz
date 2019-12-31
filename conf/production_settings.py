07614f7648bcd0352503772e7f928ccb41d57ad4
paletalex <misopitas@gmail.com>
2019-12-30 23:35:51 -0600

    gogo

--- conf/production_settings.py
+++ conf/production_settings.py
@@ -3,15 +3,19 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['albumtoon.herokuapp.com']
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'deorugc25mrl1v',
+        'USER': 'edbjxhfxcdsrbe',
+        'PASSWORD': '3189c301613912168e850409e5851f3467de48810aab3437d75b2d3f6245b9e9',
+        'HOST': 'ec2-174-129-33-217.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
